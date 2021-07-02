6730548b1f1f35ddc3165e3b214d22b0a9643b78
Pål Nyseth <paalnyseth@gmail.com>
2021-07-02 10:56:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programmeringsoppgave
+ Programmeringsoppgave Giant Leap
---
+++ src/main/java/no/nyseth/oppgave/OppgaveApplication.java
@@ -0,0 +1,13 @@
+package no.nyseth.oppgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OppgaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OppgaveApplication.class, args);
+	}
+
+}
